8b9921359d45abe1718ea060244f527d51082551
Zach Lefler <zachary.lefler@gmail.com>
2024-09-14 08:37:10 -0700

    layer switches, no combos yet

--- keyboards/keebio/iris_ce/keymaps/zlefler/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/zlefler/keymap.c
@@ -5,13 +5,18 @@
 
 enum custom_layers { _QWERTY, _LOWER, _RAISE };
 
+const uint16_t PROGMEM enter_combo[] = {KC_K, KC_L, COMBO_END};
+combo_t                key_combos[]  = {
+    COMBO(enter_combo, KC_ENT),
+
+};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
         // ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
         QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_ENT,
         // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
+        CW_TOGG, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
         // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
